3d7115bc1aae3ac802e88eef5abec6a863465506
Y4r0z <jarosl.s@yandex.ru>
2024-07-17 17:08:05 +0300

    Фикс путей

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@nextui-org/button";
-import { Link } from "@nextui-org/link";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="dark flex min-h-screen flex-col items-center justify-between p-24 text-otext">
+    <main className="dark flex min-h-screen flex-col items-center justify-cneter p-24 text-otext gap-8">
       <div>
-        <Link href="manager"><Button>Перейти в редактор пользователей</Button></Link>
+        <Link href="/manager"><Button>Перейти в редактор пользователей</Button></Link>
+      </div>
+      <div>
+        <Link href="/logs"><Button>Просмотр логов чата</Button></Link>
       </div>
     </main>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     env: {
         apiHost: 'http://92.63.178.191:3005'
     },
+    basePath: null
 };
 
 export default nextConfig;
